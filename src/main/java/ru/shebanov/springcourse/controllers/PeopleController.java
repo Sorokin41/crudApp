416ce2411ab803e20e0220ee7b793f71737304bc
Vladislav <sheba511@mail.ru>
2023-05-06 17:12:46 +0300

    Creating added

--- src/main/java/ru/shebanov/springcourse/controllers/PeopleController.java
+++ src/main/java/ru/shebanov/springcourse/controllers/PeopleController.java
@@ -4,13 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.shebanov.springcourse.dao.PersonDAO;
+import ru.shebanov.springcourse.models.Person;
 
 @Controller
 @RequestMapping("/people")
 public class PeopleController {
+	
 	private final PersonDAO personDAO;
 	
 	@Autowired
@@ -25,8 +29,20 @@ public class PeopleController {
 	}
 	
 	@GetMapping("/{id}")
-	public String show(@PathVariable("id") int id, Model model) {
+	public String show(@PathVariable("id") int id, 
+					   Model model) {
 		model.addAttribute("person", personDAO.show(id));
 		return "people/show";
+	}
+	
+	@GetMapping("/new")
+	public String newPerson(@ModelAttribute("person") Person person) {
+		return "people/new";
+	}
+	
+	@PostMapping()
+	public String create(@ModelAttribute("person") Person person) {
+		personDAO.save(person);
+		return "redirect:/people";
 	}
 }
