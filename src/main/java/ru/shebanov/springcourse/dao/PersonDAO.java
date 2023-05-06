ac96b8fbd197e448689aa8ff4715a1317c73c181
Vladislav <sheba511@mail.ru>
2023-05-06 16:43:41 +0300

    first commit

---
+++ src/main/java/ru/shebanov/springcourse/dao/PersonDAO.java
@@ -0,0 +1,29 @@
+package ru.shebanov.springcourse.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import ru.shebanov.springcourse.models.Person;
+
+@Component
+public class PersonDAO {
+	private static int PEOPLE_COUNT;
+	private List<Person> people;
+	{
+		people = new ArrayList<>();
+		people.add(new Person(++PEOPLE_COUNT, "Vladislav"));
+		people.add(new Person(++PEOPLE_COUNT, "Angelina"));
+		people.add(new Person(++PEOPLE_COUNT, "Egor"));
+		people.add(new Person(++PEOPLE_COUNT, "Volodya"));
+	}
+	
+	public List<Person> index(){
+		return people;
+	}
+	
+	public Person show(int id) {
+		return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+	}
+}
